f79a483a042469e0f02a105f8c99b7e0c20f4d6c
Orla-FM <orlandofm11@gmail.com>
2024-08-01 16:30:38 -0600

    Update index.php
    Actualización de tabla

--- PhpProject1/index.php
+++ PhpProject1/index.php
@@ -11,6 +11,29 @@ Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edi
     <body>
         
         <a>Hola</a>
+        
+            <table>
+                <thead>
+                    <tr>
+                        <th>No. List.</th>
+                        <th>Nombre</th>
+                        <th>apellidos</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                      <td>1</td>
+                      <td>Pablo</td>
+                      <td>Alvarado Macias</td>  
+                    </tr>
+                    <tr>
+                      <td>2</td>
+                      <td>Luis</td>
+                      <td>Fernandez Hernandez</td>  
+                    </tr>
+                </tbody>
+            </table>
+       
         <?php
         // put your code here
         ?>
